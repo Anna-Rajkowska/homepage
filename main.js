3192d24b6125f88a6c62e316ca7b46b781f6eebb
Anna Rajkowska <anka.rajkowska@gmail.com>
2020-05-14 18:46:21 +0200

    main page changes: css and html

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ const age = "30"
 
 console.log(firstName);
 console.log(age);
-console.log(`Hejka, nazywam się ${firstName} i mam ${age} lat.`)
+console.log(`Hejka, nazywam się ${firstName} i mam ${age} lat.`);
