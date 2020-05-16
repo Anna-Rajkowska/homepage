12ffae7622a380dff8e19b579fe4cee620939655
Anna Rajkowska <anka.rajkowska@gmail.com>
2020-05-16 14:02:31 +0200

    add journal page and js element

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const firstName = "Anna";
-const age = "30"
+const age = "30";
 
 console.log(firstName);
 console.log(age);
 console.log(`Hejka, nazywam się ${firstName} i mam ${age} lat.`);
+
+const heading = document.querySelector('.banner__header--js');
+
+heading.innerHTML = `Hejka, nazywam się ${firstName} i mam ${age} lat.`;
+
+const emptyParagraph = document.querySelector('.banner__header--js');
+emptyParagraph.innerHTML = 'Zrobiłam i uzupełniłam, ale nie mam zielonego pojęcia o co chodzi :(';
